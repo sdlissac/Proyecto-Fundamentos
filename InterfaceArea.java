ab3bd2887304429c1d0888fcc6962b06d87963fd
07GUZMAN11 <100952675+07GUZMAN11@users.noreply.github.com>
2022-06-02 19:26:27 -0500

    Delete InterfaceArea.java

--- InterfaceArea.java
+++
@@ -1,8 +0,0 @@
-package CadenaEs;
-
-public interface InterfaceArea {
-
-    public void setNext(InterfaceArea interfaceArea);
-    public InterfaceArea getNext();
-    public void clasificacion(int opcion);
-}
