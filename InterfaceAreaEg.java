62f24b54286b56d120ecbe7187a8355695293b79
07GUZMAN11 <100952675+07GUZMAN11@users.noreply.github.com>
2022-06-02 19:26:34 -0500

    Delete InterfaceAreaEg.java

--- InterfaceAreaEg.java
+++
@@ -1,7 +0,0 @@
-package CadenaEg;
-
-public interface InterfaceAreaEg {
-    public void setNext(InterfaceAreaEg interfaceAreaEg);
-    public InterfaceAreaEg getNext();
-    public void clasificacion1(int opcion);
-}
